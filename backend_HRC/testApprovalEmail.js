806af79324485fb73d85490d4b57fa8f599bed2f
Chetan Patil <cp02032006@gmail.com>
2026-07-27 23:22:00 +0530

    Backend and DataBase Updates

--- backend_HRC/testApprovalEmail.js
+++
@@ -1,9 +0,0 @@
-const { sendApprovalEmail } = require('./src/utils/emailService');
-
-async function test() {
-  console.log('Testing sendApprovalEmail...');
-  const success = await sendApprovalEmail(process.env.SMTP_USER, 'Test User');
-  console.log('Success:', success);
-}
-
-test();
